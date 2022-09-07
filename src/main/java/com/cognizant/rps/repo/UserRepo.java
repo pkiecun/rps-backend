2fb05a59060e4766a2a10a510fa0d49d5d599355
Tsuchiyama <2211118@cognizant.com>
2022-09-07 11:56:27 -0700

    Kinda broken, but probably just here

--- src/main/java/com/cognizant/rps/repo/UserRepo.java
+++ src/main/java/com/cognizant/rps/repo/UserRepo.java
@@ -4,6 +4,13 @@ import com.cognizant.rps.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepo extends JpaRepository<User, Integer> {
+
+    User findByUsername(String username);
+    User save(User person);
+    List<User> findAll();
+
 }
---
+++ src/main/java/com/cognizant/rps/service/UserService.java
@@ -0,0 +1,67 @@
+package com.cognizant.rps.service;
+
+import com.cognizant.rps.models.User;
+import com.cognizant.rps.repo.UserRepo;
+import com.cognizant.rps.util.Utility;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
+@Transactional
+public class UserService {
+
+    private Utility jwt;
+
+    private UserRepo ur;
+
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepo ur, Utility jwt){
+        this.ur = ur;
+        this.jwt = jwt;
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
+
+
+    public String loggedIn(User attempt){
+
+        User getUser = ur.findByUsername(attempt.getUsername());
+        boolean verify = passwordEncoder.matches(attempt.getPassphrase(), getUser.getPassphrase());
+
+        if (verify) {
+            getUser.setLogin(true);
+            ur.save(getUser);
+            return "Bearer " + jwt.generateToken(getUser);
+        } else {
+            //throw new InvalidCredentialsException();
+            return "Wrong Password";
+        }
+    }
+
+    public String register(User attempt){
+        List<User> registry = ur.findAll();
+        Set<String> registered = new HashSet<>();
+        for(User name : registry){
+            registered.add(name.getUsername());
+        }
+        System.out.println("In register service");
+        User user = new User(0, attempt.getUsername(), attempt.getPassphrase(), true);
+        if(registered.add(user.getUsername())){
+            ur.save(user);
+            return "Bearer " + jwt.generateToken(user);
+        }else{
+//            throw new InvalidCredentialsException();
+            return "EXCEPTION ON LINE 61";
+        }
+    }
+
+}
+
