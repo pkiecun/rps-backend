8d4971cf42ff5483bc75354b45f184028dd14390
unknown <pkiecun@gmail.com>
2022-09-08 16:06:51 -0400

    newmatchmodel

---
+++ src/main/java/com/cognizant/rps/models/Match.java
@@ -0,0 +1,16 @@
+package com.cognizant.rps.models;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Match {
+
+    private int limit;
+    private int count;
+    private int move;
+
+}
